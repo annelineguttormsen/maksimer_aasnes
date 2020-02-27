8a5c54ee4ca6149d8f3e5bff7e6dd850e3a63118
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2020-02-27 15:47:39 +0100

    header v.0.01
    searchbar og drop down meny

--- dist/js/mainscript.js
+++ dist/js/mainscript.js
@@ -1,6 +1,70 @@
+//meny
+//variabler for dropdown meny
+$ddMenuBtn = $(".header__logo--menu");
+$ddMenu = $(".dd-menu ul");
+
+//når hamburger ikonet trykkes skal dd meny poppe fram
+//sett fokus på dd meny
+//fjern eventlistener på hamburger ikon
+$ddMenuBtn.on("click",openMenu);
+
+//skjul dd meny når fokuset fjernes
+//legg tilbake event listener på hamburger ikon
+$ddMenu.on("blur",closeMenu);
+
+function openMenu() {
+	$ddMenu.addClass("dd-menu--open");
+	$ddMenu.focus();
+	$ddMenuBtn.off();
+}
+
+function closeMenu() {
+	$ddMenu.removeClass("dd-menu--open");
+	$ddMenuBtn.on("click",openMenu);
+}
+
+//spoof søkefunksjon
+//variabler med elementer fra siden
+$searchIcon = $(".header__logo--search");
+$form = $(".header__form");
+$formInput = $(".header__form--input");
+
+//når søkeikonet trykkes, skal input field vises
+//input får fokus via addSearchBar, hvis fokus
+//går bort, skal input field bli skjult
+$searchIcon.on("click", addSearchBar);
+$formInput.on("blur", hideSearchBar);
+
+
+function addSearchBar() {
+	//fjern eventlistener fra søkeikonet
+	$searchIcon.off();
+	$form.css("display","inline-block");
+	let timeout = setTimeout(
+		function(){
+			$formInput.css("width","200px")
+		},10);
+	//setter fokus på input inni form elementet
+	$formInput.focus();
+}
+
+function hideSearchBar() {
+	//vent med å skjule form elementet så input field
+	//får spilt ut sin transition
+	let timeout = setTimeout(
+		function() {
+			$form.css("display","none");
+			//legg tilbake eventlistener på søkeikonet
+			$searchIcon.on("click", addSearchBar);
+		}, 200
+	);
+	$formInput.css("width","0px");
+}
+
+
 //enkelt slideshow
 
-const images = 
+/*const images = 
 	["https://www.apple.com/newsroom/images/product/apps/lifestyle/Apple-Watch-records-ski-workouts-02282018_big.jpg.large.jpg",
 	"https://images.interactives.dk/kvinde-ski-MwwN38y8QHj49z0kfKo1FQ.jpg?auto=compress&ch=Width%2CDPR&dpr=2.63&ixjsv=2.2.4&q=38&rect=113%2C4%2C5534%2C3831&w=430",
 	"media/img/AI_image.jpg"];
@@ -17,7 +81,7 @@ function changeBackground() {
 		$(".slideshow").css({"background":"url(" + images[n] + ")no-repeat center center","background-size":"100%"});
 	}
 	console.log("bakgrunnsbildet er nå " + images[n]);
-}
+}*/
 /*
 |￣￣￣￣￣ | 
 |   BLESS  | 
