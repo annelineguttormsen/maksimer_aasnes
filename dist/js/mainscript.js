b587109a4be63cccd2e2d81eebe23a327febb6a8
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2020-02-29 17:52:43 +0100

    dd-menu
    Fjernet "blinking" av dd-menu om man prøver å toggle hamburger ikon

--- dist/js/mainscript.js
+++ dist/js/mainscript.js
@@ -1,7 +1,7 @@
 //meny
-//variabler for dropdown meny
-const $ddMenuBtn = $(".header__logo--menu");
+//variabler for dropdown meny og hamburger ikon
 const $ddMenu = $(".dd-menu");
+const $ddMenuBtn = $(".header__logo--menu");
 
 //når hamburger ikonet trykkes skal dd meny poppe fram
 //sett fokus på dd meny
@@ -10,31 +10,36 @@ $ddMenuBtn.on("mouseup",openMenu);
 
 //skjul dd meny når fokuset fjernes
 //legg tilbake event listener på hamburger ikon
-$ddMenu.on("blur",blurCloseMenu);
+$ddMenu.on("blur",closeMenu);
 
 function openMenu() {
+	//i tilfelle meny er åpent og hamburger ikon
+	//har eventlistener, bare lukk menyen om
+	//den har --open klasse
 	if($ddMenu.hasClass("dd-menu--open")) {
 		closeMenu();
-		console.log("meny er allerede åpen");
 	} else {
 		$ddMenu.addClass("dd-menu--open");
-		console.log("meny er ikke åpen");
+		//fjern eventlistener fra hamburger ikonet
+		//sett fokus på menyen slik at blur funksjonen kan brukes
+		$ddMenuBtn.off();
 		$ddMenu.focus();
 	}
 }
 
 function closeMenu() {
 	$ddMenu.removeClass("dd-menu--open");
-	console.log("closeMenu tilkalt");
-}
-
-function blurCloseMenu() {
-	$ddMenu.removeClass("dd-menu--open");
-	console.log("closeMenu tilkalt");
+	//blur funksjonen aktiveres med en gang
+	//hamburger ikonet blir trykket, og dette 
+	//fører til "blinking", så vi venter 300 millisekunder
+	//på å sette tilbake eventlistener på knappen
+	let timeout = setTimeout(function(){
+		$ddMenuBtn.on("mouseup",openMenu);
+	},300);
 }
 
 //spoof søkefunksjon
-//variabler med elementer fra siden
+//variabler med elementer for søkeikon og form elementer
 const $searchIcon = $(".header__logo--search");
 const $form = $(".header__form");
 const $formInput = $(".header__form__input");
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Maksimer Åsnes kodetest
 
-Dette er en kodetest basert ut fra PDF og AI-filer fra Maksimer. Alle bildene og andre filer brukt i dette prosjektet kommer fra kodetesten sin zip-fil, bortsett fra "checkmark"-bildet og fontene kommer fra Google Fonts.
+Dette er en kodetest basert ut på PDF og AI-filer fra Maksimer. Alle bildene og andre filer brukt i dette prosjektet kommer fra kodetesten sin zip-fil, bortsett fra "checkmark"-bildet og fontene som er brukt er fra Google Fonts.
 
 Kompilert CSS fil og fungerende HTML fil er plassert i dist-mappen.
